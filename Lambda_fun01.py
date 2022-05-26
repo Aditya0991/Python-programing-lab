2951a2a6c6b048f91822ecd9454b6021b141fc6d
Aditya0991 <92169367+Aditya0991@users.noreply.github.com>
2022-05-26 23:25:15 +0530

    Add files via upload

---
+++ Lambda_fun01.py
@@ -0,0 +1,4 @@
+'''1. Write a Python program to find if a given string starts 
+with a given character using Lambda.'''
+starts_with = lambda x: True if x.startswith('I') else False
+print(starts_with('I'))
