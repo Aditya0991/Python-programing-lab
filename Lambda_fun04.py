071da187496df4391c0975f833fbea02cfb7c1f7
Aditya0991 <92169367+Aditya0991@users.noreply.github.com>
2022-05-26 23:26:22 +0530

    Add files via upload

---
+++ Lambda_fun04.py
@@ -0,0 +1,5 @@
+'''Q4. Write a Python program to create a function that 
+takes one argument, and that argument will be multiplied with 
+an unknown given number.'''
+ann = lambda x:15*x
+print(ann(4))
