58bb67fd54a1f1f727f56c6ada7895d5a0b2fa6b
Aditya0991 <92169367+Aditya0991@users.noreply.github.com>
2022-05-31 19:54:47 +0530

    Add files via upload

---
+++ IF ELIF ELSE03.py
@@ -0,0 +1,7 @@
+num1=int(input("Enter the number1: "))
+num2=int(input("Enter the number2: "))
+if(num1>num2):
+    sub=num1-num2
+else:
+    sub=num2-num1
+print(f"substraction of smaller number from greater number {sub}")
